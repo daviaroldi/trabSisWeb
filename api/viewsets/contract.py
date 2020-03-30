4299b57c67285ab0d5e82561398899a638f01329
Davi Aroldi <davi.aroldi@gmail.com>
2020-03-29 21:12:30 -0300

    changes

--- api/viewsets/contract.py
+++ api/viewsets/contract.py
@@ -4,12 +4,12 @@ from django.db import models
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated
+from ..serializers.contract import ContractSerializer
 from ..models.contract import Contract
 from _datetime import datetime
 import json
 
 from rest_framework import viewsets
-from ..serializers.contract import ContractSerializer
 
 class ContractViewSet(viewsets.ModelViewSet):
     queryset = Contract.objects.all().order_by('-title')
