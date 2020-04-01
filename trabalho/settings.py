4abf36a0431caf4347a9affc6f7f028a79ab5c88
Davi Aroldi <davi.aroldi@gmail.com>
2020-04-01 01:06:12 -0300

    updating settings

--- trabalho/settings.py
+++ trabalho/settings.py
@@ -59,6 +59,9 @@ CORS_ALLOW_CREDENTIALS = True
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('oauth2_provider.contrib.rest_framework.OAuth2Authentication',),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
 }
 
 OAUTH2_PROVIDER = {
