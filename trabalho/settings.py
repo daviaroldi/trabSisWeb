1f74802f66a0ebbc3af1b426ffdd3d18ccd49e25
Davi Aroldi <davi.aroldi@gmail.com>
2020-03-31 23:10:05 -0300

    fixing some sintax errors

--- trabalho/settings.py
+++ trabalho/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheader.middleware.CorsMiddleware','
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
